3cb6dfcbe173a78883fde4152f313415462fdceb
Alexander Gattinger <nscgattinger@Alexanders-MacBook-Pro.local>
2019-06-24 11:20:36 +0200

    hi

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<script
+		  src="https://code.jquery.com/jquery-3.4.1.min.js"
+		  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+		  crossorigin="anonymous"></script>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
+
+
+		<link rel="stylesheet" href="/css/master.css">
+		<script type="text/javascript" src="js/master.js">
+
+		</script>
+
+		<meta charset="utf-8">
+		<title>Doctors Hate Me</title>
+	</head>
+	<body>
+		<script type="text/javascript">
+	alert("hi");
+
+	</script>
+
+	</body>
+</html>
